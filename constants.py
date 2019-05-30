f972976575403f38b554b36a49a373c88bb56308
Mike Ford <mikejford@gmail.com>
2019-05-30 00:17:03 -0500

    Update to Dungeon List command and beginning to handle Add command. Aliases and constants updated.

---
+++ constants.py
@@ -0,0 +1,17 @@
+DUNGEON_LIST = {
+    'Atal\'Dazar': ['AD', 'Atal'],
+    'Freehold': ['FH'],
+    'King\'s Rest': ['KR'],
+    'Shrine of the Storm': ['SOTS', 'Shrine'],
+    'Siege of Boralus': ['SB', 'SOB', 'Siege'],
+    'Temple of Sethraliss': ['TOS', 'Temple'],
+    'The MOTHERLODE!!': ['ML', 'Mother', 'Lode'],
+    'The Underrot': ['UR', 'Underrot'],
+    'Tol Dagor': ['TD'],
+    'Waycrest Manor': ['WM', 'Waycrest']
+}
+
+GITHUB_URL = 'https://github.com/mikejford/keystone'
+GITHUB_ICON_URL = 'https://image.flaticon.com/icons/svg/25/25231.svg'
+AFFIX_URL = 'https://raider.io/api/v1/mythic-plus/affixes?region=us&locale=en'
+KEYSTONE_ICON_URL = 'https://wow.zamimg.com/images/wow/icons/large/inv_relics_hourglass.jpg'
--- keystone_bot.py
+++ keystone_bot.py
@@ -1,17 +1,55 @@
 import discord
 from discord.ext import commands
+from constants import DUNGEON_LIST
 
 token = open("token.txt", "r").read().strip()
 
-bot = commands.Bot(command_prefix='!')
+name_len = max(map(lambda name: len(name), DUNGEON_LIST.keys()))
+abbr_len = max(map(lambda abbrs: len(', '.join(abbrs)), DUNGEON_LIST.values()))
+
+dungeon_abbr_list = {}
+for name, abbrs in DUNGEON_LIST.items():
+    dungeon_abbr_list.update(dict.fromkeys([a.lower() for a in abbrs], name))
+print(repr(dungeon_abbr_list))
+
+class InvalidDungeon(commands.CheckFailure):
+    pass
+
+bot = commands.Bot(command_prefix='!ks ')
 
 @bot.event
 async def on_ready():
     print('Logged in as {0.user}'.format(bot))
 
-@bot.command()
-async def add(ctx, dungeon: str, lvl: int, character=None):
+@bot.command(help='Adds your keystone to the list', aliases=['replace', '+'])
+async def add(ctx, dungeon: str, lvl: int, character: str = None):
+    if dungeon.lower() not in dungeon_abbr_list.keys():
+        raise InvalidDungeon('Dungeon abbreviation not found. Use `!ks list` to see the list of acceptable abbreviations')
+
     name = character or ctx.author.display_name
+    # ctx.invoked_with reveals how the command was called
     await ctx.send('{} {} added for {}'.format(dungeon, lvl, name))
+
+@add.error
+async def add_error(ctx, error):
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send(error)
+
+@bot.command(help='Removes your saved keystone from the list', aliases=['del', '-'])
+async def remove(ctx, character: str = None):
+    name = character or ctx.author.display_name
+    await ctx.send('{} removed their key'.format(name))
+
+@bot.command(help='', aliases=['keys', 'all'])
+async def list(ctx):
+    await ctx.send('List of keystones')
+
+@bot.command(help='Shows a list of dungeons and acceptable abbreviations')
+async def dungeons(ctx):
+    header = '| {:<{name_len}} | {:<{abbr_len}} |'.format('Dungeon', 'Abbreviations', name_len=name_len, abbr_len=abbr_len)
+    msg = header + '\n' + ('-' * len(header)) + '\n'
+    for name, abbr in DUNGEON_LIST.items():
+        msg += '| {:<{name_len}} | {:<{abbr_len}} |\n'.format(name, ', '.join(abbr), name_len=name_len, abbr_len=abbr_len)
+    await ctx.send('```' + msg + '```')
 
 bot.run(token)
