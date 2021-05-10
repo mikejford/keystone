24c46ee01943a66179fb1b799a0ab9c81b97178b
Mike Ford <mikejford@gmail.com>
2021-05-09 22:31:36 -0500

    Add query to remove expired keys from storage. Finished noting all jsonpickle related content for removal.

--- keystone.py
+++ keystone.py
@@ -1,16 +1,18 @@
 import requests
-import jsonpickle # remove after tinydb is working
-
 from tinydb import TinyDB, Query
-
 from datetime import datetime, timedelta
-from threading import Lock
 from constants import AFFIX_URL
+
 from keys_api import KeysApi
+
+# remove after tinydb is working
+import jsonpickle 
+from threading import Lock
 
 lock = Lock()
 keys_api = KeysApi()
 
+# remove after tinydb is working
 def keystone_insort_right(a, x, lo=0, hi=None):
     if hi is None:
         hi = len(a)
@@ -34,9 +36,9 @@ class Keystone():
 class KeystoneStorage():
     def __init__(self):
         self.db = TinyDB('keystonedb.json')
+        self._reset_cache()
 
         # jsonpickle related operations
-        self._reset_cache()
         with lock:
             try:
                 with open('keystone_cache.json', 'r') as f:
@@ -47,30 +49,39 @@ class KeystoneStorage():
             except FileNotFoundError:
                 print('No cache data found.')
 
-    def find_key_by_name(self, guild_id, name):
-        for index, keystone in enumerate(self.guilds[guild_id]):
-            if keystone.owner == name:
-                return index
-        return None
-
     def _reset_cache(self):
-        # use datetime.fromisoformat(str) to evaluate keys in the tinydb
-
-        self.load_affixes()
-        self.guilds = {}
+        # Determine next US server reset time (UTC)
         now = datetime.utcnow()
         next_tuesday = now.replace(hour=15, minute=0, second=0, microsecond=0) + timedelta(days=(1-now.weekday()) % 7)
         if now > next_tuesday:
             next_tuesday += timedelta(days=7)
         self.timestamp = next_tuesday
 
+        # Remove expired keys from storage
+        key = Query()
+        self.db.remove(key.invalid_after.test(KeystoneStorage._test_expired_key, now))
+
+        # Load weekly affixes
+        self.load_affixes()
+
+        # jsonpickle related operations
+        self.guilds = {}
+
+    def _test_expired_key(val, now):
+        # test function used by a query to determine if a key is expired
+        return datetime.fromisoformat(val) < now
+
+    # guild_id can be removed with jsonpickle removal
     def check_cache(self, guild_id):
         if datetime.utcnow() > self.timestamp:
             self._reset_cache()
+            # jsonpickle related operations
             self._save_cache()
+        # jsonpickle related operations
         if guild_id not in self.guilds:
             self.guilds[guild_id] = []
 
+    # jsonpickle related operations
     def _save_cache(self):
         with lock:
             with open('keystone_cache.json', 'w') as f:
@@ -78,7 +89,6 @@ class KeystoneStorage():
 
     def add_key(self, guild_id, user_id, dungeon, lvl, name):
         self.check_cache(guild_id)
-        key = Keystone(dungeon, lvl, name, user_id, self.timestamp)
         
         key_dict = {
                 'guild_id': guild_id,
@@ -90,12 +100,16 @@ class KeystoneStorage():
                 }
 
         key_query = Query()
-        self.db.upsert(key_dict, (key_query.guild_id == guild_id) & (key_query.owner == name))
+        added = self.db.upsert(key_dict, (key_query.guild_id == guild_id) & (key_query.owner == name))
+
+        # caller expects a Keystone return value 
+        # tinydb upsert returns a list of removed doc ids
 
         # move this to a separate handler registered with the add command
         # keys_api.post_key(user_id, name, dungeon, lvl)
 
         # jsonpickle related operations
+        key = Keystone(dungeon, lvl, name, user_id, self.timestamp)
         if self.find_key_by_name(guild_id, name) is not None:
             index = self.find_key_by_name(guild_id, name)
             self.guilds[guild_id][index] = key
@@ -106,12 +120,15 @@ class KeystoneStorage():
 
     def remove_key(self, guild_id, user_id, name):
         self.check_cache(guild_id)
-        key = None
 
         key_query = Query()
-        self.db.remove((key_query.guild_id == guild_id) & (key_query.owner == name))
+        removed = self.db.remove((key_query.guild_id == guild_id) & (key_query.owner == name))
+
+        # caller expects a Keystone return value 
+        # tinydb remove returns a list of removed doc ids
 
         # jsonpickle related operations
+        key = None
         index = self.find_key_by_name(guild_id, name)
         if index is not None \
                 and user_id == self.guilds[guild_id][index].user_id:
@@ -119,9 +136,16 @@ class KeystoneStorage():
         self._save_cache()
         return key
 
+    # jsonpickle related method
+    def find_key_by_name(self, guild_id, name):
+        for index, keystone in enumerate(self.guilds[guild_id]):
+            if keystone.owner == name:
+                return index
+        return None
+
     def get_keys_by_guild(self, guild_id):
-        key_query = Query()
-        return self.db.search(key_query.guild_id == guild_id)
+        key = Query()
+        return self.db.search(key.guild_id == guild_id)
 
     def load_affixes(self):
         r = requests.get(AFFIX_URL, timeout=2)
