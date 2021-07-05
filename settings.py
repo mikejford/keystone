4fb619e2924a948ce0d75faf12919fac24c81674
Mike Ford <mikejford@gmail.com>
2021-07-05 13:48:36 -0500

    Converting constants and other configuration into JSON settings

---
+++ settings.py
@@ -0,0 +1,35 @@
+import json
+import requests
+
+with open('settings.json', 'r' as f:
+    settingsjson = Settings(json.loads(f.read()))
+
+class Settings(BaseSettings):
+    pass
+
+
+class BaseSettings(dict):
+    def __init__(self, *args):
+        super(BaseSettings, self).__init__()
+
+        for arg in args:
+            for k, v in arg.items():
+                v = BaseSettings(v) if isinstance(v, dict) else v
+                self.__setattr__(k, v)
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+
+    def __setitem__(self, key, value):
+        super(BaseSettings, self).__setitem__(key, value)
+        self.__dict__.update({key: value})
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(BaseSettings, self).__delitem__(key)
+        del self.__dict__[key]
