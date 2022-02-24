1816f5a05523a66fdc2d5250963fb33793079f7e
Mike Ford <mikejford@gmail.com>
2022-02-23 23:50:59 -0600

    Add Tazavesh dungeons and new keystone icon (#4)

--- constants.py
+++ constants.py
@@ -7,6 +7,8 @@ DUNGEON_LIST = {
     'Spires of Ascension': ['SOA', 'Spires'],
     'The Necrotic Wake': ['NW'],
     'Theater of Pain': ['TOP', 'Theater'],
+    'Tazavesh: Streets of Wonder': ['SOW', 'Streets'],
+    'Tazavesh: So\'leah\'s Gambit': ['SG', 'TSG', 'Gambit'],
 }
 
 DUNGEON_ABBR_LIST = {}
@@ -19,5 +21,5 @@ MIN_KEYSTONE_LEVEL = 2
 GITHUB_URL = 'https://github.com/mikejford/keystone'
 GITHUB_ICON_URL = 'https://image.flaticon.com/icons/svg/25/25231.svg'
 AFFIX_URL = 'https://raider.io/api/v1/mythic-plus/affixes?region=us&locale=en'
-KEYSTONE_ICON_URL = 'https://wow.zamimg.com/images/wow/icons/large/inv_relics_hourglass.jpg'
+KEYSTONE_ICON_URL = 'https://wow.zamimg.com/images/wow/icons/large/inv_relics_hourglass_02.jpg'
 KEYSAPI_ADD_KEY_URL = 'https://espkrandardsaskunclezar20200801100540.azurewebsites.net/api/keys'
